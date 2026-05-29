ac552e6c57eaf4893d45c4edc03a6fd306d24f7e
romainsessa <romainsessa@gmail.com>
2026-05-29 14:29:11 +0200

    fix TeacherController issue : move teacherMapper to TeacherService

--- back/src/main/java/com/openclassrooms/starterjwt/controllers/TeacherController.java
+++ back/src/main/java/com/openclassrooms/starterjwt/controllers/TeacherController.java
@@ -1,7 +1,6 @@
 package com.openclassrooms.starterjwt.controllers;
 
-import com.openclassrooms.starterjwt.mapper.TeacherMapper;
-import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.dto.TeacherDto;
 import com.openclassrooms.starterjwt.services.TeacherService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,35 +13,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/teacher")
 public class TeacherController {
-    private final TeacherMapper teacherMapper;
-    private final TeacherService teacherService;
+	private final TeacherService teacherService;
 
+	public TeacherController(TeacherService teacherService) {
+		this.teacherService = teacherService;
+	}
 
-    public TeacherController(TeacherService teacherService,
-                             TeacherMapper teacherMapper) {
-        this.teacherMapper = teacherMapper;
-        this.teacherService = teacherService;
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<TeacherDto> findById(@PathVariable("id") String id) {
+		try {
+			TeacherDto teacherDto = this.teacherService.findById(Long.valueOf(id));
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable("id") String id) {
-        try {
-            Teacher teacher = this.teacherService.findById(Long.valueOf(id));
+			if (teacherDto == null) {
+				return ResponseEntity.notFound().build();
+			}
 
-            if (teacher == null) {
-                return ResponseEntity.notFound().build();
-            }
+			return ResponseEntity.ok().body(teacherDto);
+		} catch (NumberFormatException e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
 
-            return ResponseEntity.ok().body(this.teacherMapper.toDto(teacher));
-        } catch (NumberFormatException e) {
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
-    @GetMapping()
-    public ResponseEntity<?> findAll() {
-        List<Teacher> teachers = this.teacherService.findAll();
-
-        return ResponseEntity.ok().body(this.teacherMapper.toDto(teachers));
-    }
+	@GetMapping()
+	public ResponseEntity<List<TeacherDto>> findAll() {
+		List<TeacherDto> teachers = this.teacherService.findAll();
+		return ResponseEntity.ok().body(teachers);
+	}
 }
--- back/src/main/java/com/openclassrooms/starterjwt/services/TeacherService.java
+++ back/src/main/java/com/openclassrooms/starterjwt/services/TeacherService.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.starterjwt.services;
 
-import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.dto.TeacherDto;
+import com.openclassrooms.starterjwt.mapper.TeacherMapper;
 import com.openclassrooms.starterjwt.repository.TeacherRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,17 +9,20 @@ import java.util.List;
 
 @Service
 public class TeacherService {
+	
+	private final TeacherMapper teacherMapper;
     private final TeacherRepository teacherRepository;
 
-    public TeacherService(TeacherRepository teacherRepository) {
+    public TeacherService(TeacherRepository teacherRepository, TeacherMapper teacherMapper) {
         this.teacherRepository = teacherRepository;
+        this.teacherMapper = teacherMapper;
     }
 
-    public List<Teacher> findAll() {
-        return this.teacherRepository.findAll();
+    public List<TeacherDto> findAll() {
+        return this.teacherMapper.toDto(this.teacherRepository.findAll());
     }
 
-    public Teacher findById(Long id) {
-        return this.teacherRepository.findById(id).orElse(null);
+    public TeacherDto findById(Long id) {
+        return this.teacherMapper.toDto(teacherRepository.findById(id).orElse(null));
     }
 }
