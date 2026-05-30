38579571b84dadcd8a3088bda35ea915473d0c4f
romainsessa <romainsessa@gmail.com>
2026-05-30 18:49:05 +0200

    fix any

--- front/src/app/pages/sessions/components/detail/detail.component.ts
+++ front/src/app/pages/sessions/components/detail/detail.component.ts
@@ -49,7 +49,7 @@ export class DetailComponent implements OnInit {
   public delete(): void {
     this.sessionApiService
       .delete(this.sessionId)
-      .subscribe((_: any) => {
+      .subscribe((_: void) => {
           this.matSnackBar.open('Session deleted !', 'Close', { duration: 3000 });
           this.router.navigate(['sessions']);
         }
